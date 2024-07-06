15bd6d29d97e61c4c070241d1c22d60d69e403c9
frockett <crockett.d.ford@gmail.com>
2024-07-06 17:55:06 +0800

    related posts box at bottom of blog post

--- src/scripts/Utils.js
+++ src/scripts/Utils.js
@@ -1,7 +1,4 @@
-
-
-function sortPosts(posts)
-{
+function sortPosts(posts) {
   const postCount = posts.length;
 
   const sortByDate = () => {
@@ -10,7 +7,24 @@ function sortPosts(posts)
     );
   };
 
-  return { sortByDate, postCount };
+  const getRelatedPosts = (tags) => {
+    if (!Array.isArray(tags)) {
+      tags = [tags];
+    }
+
+    const allRelatedPosts = [];
+
+    tags.forEach((tag) => {
+      const relatedPosts = posts.filter((post) =>
+        post.data.tags?.includes(tag)
+      );
+      allRelatedPosts.push(...relatedPosts);
+    });
+
+    return [...new Set(allRelatedPosts)];
+  };
+
+  return { sortByDate, postCount, getRelatedPosts };
 }
 
 export { sortPosts };
