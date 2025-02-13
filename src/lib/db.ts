eb7d0dd87514a4fd8ae30e03d8f56573c74e94cf
frockett <crockett.d.ford@gmail.com>
2025-02-13 23:19:49 +0800

    refactored index into some components, added ssr adapter, created server component

---
+++ src/lib/db.ts
@@ -0,0 +1,10 @@
+import Database from 'better-sqlite3';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
+
+// Connect to your local file—this file will persist on disk
+const sqliteDB = new Database('./data/astro.db', {
+  // You can add options like { verbose: console.log } for debugging
+});
+
+// Initialize Drizzle ORM with your connection
+export const dbClient = drizzle(sqliteDB);
